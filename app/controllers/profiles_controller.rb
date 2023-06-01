4376d147c5dee3fbd076827b2a8bb97f0ce4577b
MatsuiSeiya <seiya.matsui@sbworks.jp>
2023-06-01 19:40:26 +0900

    create profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,15 @@
 class ProfilesController < ApplicationController
-  def index
-    render action: :index
+  def show
+    if user_signed_in?
+      @profile = current_user.profile 
+    else
+      render :show
+    end
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
