8b7995b46f99006f9e84fc11253d0a1a3baf83b9
Seiya Matsui <hallelujah.m0214@gmail.com>
2023-10-16 18:47:38 +0900

    create user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,35 @@
 class UsersController < ApplicationController
-  def top
+  before_action :authenticate_user!, only: [:top, :show, :edit]
+
+  def top 
+    
   end
+
+  def show
+    @user = current_user
+    # @user.id = current_user.id
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.introduction = params[:introduction]
+
+    if @user.update(user_params)
+      flash[:notice] = "自己紹介文を編集しました"
+      redirect_to user_path(current_user)
+    else
+      flash.now[:alert] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:introduction, :avatar)
+  end
+
 end
