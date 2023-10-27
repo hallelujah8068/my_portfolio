194d12ad57542efd6c88710ad39d87624bbe127b
Seiya Matsui <hallelujah.m0214@gmail.com>
2023-10-28 02:11:09 +0900

    user skill destroy

---
+++ app/models/skill.rb
@@ -0,0 +1,7 @@
+class Skill < ApplicationRecord
+  belongs_to :user
+  enum category: { backend: 0, frontend: 1, infrastructure: 2 }
+
+  validates :skill, presence: true
+  validates :learning_time, presence: true, numericality: { greater_than_or_equal_to: 1 }
+end
